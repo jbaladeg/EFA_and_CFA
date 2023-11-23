4863f6268c4fb3229a96e40de35d8fee73e30544
Jenny <100955286+jbaladeg@users.noreply.github.com>
2023-11-23 11:59:07 +0000

    Add files via upload
    The procedure I followed to make the data frame for this activity

---
+++ base_efa_cfa_github.R
@@ -0,0 +1,25 @@
+#new data frame for efa_cfa_github
+
+#create a database where there are 3 variables with moderate-high correlations between them. 
+#Create another 3 variables that meet the same condition between them. 
+#And finally, create another variable that has low correlation indices between all the variables.
+
+
+set.seed(123)
+#Create a covariance matrix
+sigma.1 <- rbind(c(1, 0.4, 0.5, 0.1, 0.3, 0.1, 0.01),
+                 c(0.4, 1, 0.5, 0.2, 0.4, 0.2, 0.02),
+                 c(0.5, 0.5, 1, 0.3, 0.1, 0.2, 0.01),
+                 c(0.1, 0.2, 0.3, 1, 0.6, 0.7, 0.02),
+                 c(0.3, 0.4, 0.1, 0.6, 1, 0.5, 0.03),
+                 c(0.1, 0.2, 0.2, 0.7, 0.5, 1 , 0.04),
+                 c(0.01, 0.02, 0.01, 0.002, 0.03, 0.04, 1))
+
+#creates the average of the vectors 
+mu.1 <- c(10,5,9,7,6,500,1)
+
+#create tha data frame
+df <- as.data.frame(mvrnorm(n=500, mu = mu.1, Sigma = sigma.1))
+
+#export data frame
+exporta.archivo.fnc(df, nombre = 'df_efa_cfa_github_3')
